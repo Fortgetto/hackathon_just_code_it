05beec28f89d4b688de46f1a9f19d5ddd8a8aee9
Fortgetto <fotvorkot@gmail.com>
2017-11-19 04:23:51 +0700

    update js

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+const results = new Vue({
+    el:'#results',
+    data:{
+        articles:articles,
+        favs:favourites,
+    },
+    methods:{
+        pop_panel:function () {
+
+        },
+        table_opts:function () {
+            
+        },
+        sort:function () {
+            
+        }
+    }
+});
