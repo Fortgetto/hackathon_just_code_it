715706fc566f92054a4d026f1ab09f3f49d732b1
morozovnikita <nimokirotazov@gmail.com>
2017-11-19 09:01:49 +0700

    add modal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" href="{{asset('css/fa/font-awesome.min.css')}}">
     <script src="{{asset('js/vue/vue.js')}}"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Titile</title>
 </head>
 <body>
--- resources/views/tabs/results.blade.php
+++ resources/views/tabs/results.blade.php
@@ -45,6 +45,35 @@
         </div>
     </div>
 </div>
+<div>
+
+    <div class="container">
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Booooooooooom</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Modal Heaader</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Booooooooooooom modal</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+</div>
 <script>
     var articles = {!!$articles->toJson()!!};
     var favourites = {!!$favs->toJson()!!};
