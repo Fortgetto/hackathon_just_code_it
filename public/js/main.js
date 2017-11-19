596158bada08bc2b869484c4b5147f899df67f76
Fortgetto <fotvorkot@gmail.com>
2017-11-19 14:24:57 +0700

    add modal

--- public/js/main.js
+++ public/js/main.js
@@ -4,25 +4,38 @@ const results = new Vue({
         results:articles,
         favs:favourites,
         table_opts:{
-            data:1,
-            rait:1,
-            keywords:1,
-            article:1,
-            links:1,
-            logo:1,
-            favs:1,
-            del:1
-        }
+            time:{name:'Дата',val:1},
+            rate:{name:'Рэйтинг',val:1},
+            keywords:{name:'Ключевые слова',val:1},
+            text:{name:'Статьи',val:1},
+            link:{name:'Ссылки на новость',val:1},
+            // logo:{name:'Логотип',val:1},
+            favs:{name:'Избранное',val:1},
+            del:{name:'Удаление',val:1}
+        },
+        tab_opts:false
     },
     methods:{
         pop_panel:function () {
 
         },
-        table_opts:function () {
-            
-        },
+        // table_opts:function () {
+        //
+        // },
         sort:function () {
             
+        },
+        set_table:function (e) {
+            (e.checked)?this.table_opts[e.id].val=0:this.table_opts[e.id].val=1;
+        },
+        colored:function (v) {
+            var col = 'green';
+            if(v > 500){
+                col = 'big-red'
+            }else if(v > 100){
+                col = 'red'
+            };
+            return col
         }
     }
 });
--- resources/views/tabs/results.blade.php
+++ resources/views/tabs/results.blade.php
@@ -8,10 +8,18 @@
             <a href="javascript:location.href = '/?sortingDate'" class="ctrl-buttons date" @click="sort()">Дата <i class="fa fa-arrow-circle-o-down"></i></a>
             <a href="javascript:location.href = '/?sortingRate'" class="ctrl-buttons rait" @click="sort()">Рейтинг<i class="fa fa-arrow-circle-o-up"></i></a>
             <a href="javascript:void(0)" @click="pop_panel()" class="ctrl-buttons opts fa fa-cog" title="настройки"></a>
-            <div v-if="table_opts">
-            <a href="javascript:void(0)" @click="table_opts()" class="ctrl-buttons date fa fa-reorder" title="настройки таблицы"></a>
+            <div v-cloak v-if="tab_opts" class="dropdown-wrap">
+                <div class="wrrp">
+                    <a href="javascript:void(0)" @click="tab_opts = false" class="ctrl-buttons date fa fa-reorder" title="настройки таблицы"></a>
+                    <a href="javascript:void(0)" @click="tab_opts = false" class="ctrl-buttons date fa fa-remove" title="закрыть"></a>
+
+                </div>
+                <div class="inputs-wrp" v-for="(opts,$k) in table_opts">
+                    <label :for="$k">@{{ opts.name }}</label>
+                        <input :id="$k" type="checkbox" :checked="(opts)?'true':'false'" @click="set_table($event.target)">
+                </div>
             </div>
-            <a v-else href="javascript:void(0)" @click="table_opts()" class="ctrl-buttons date fa fa-reorder" title="настройки таблицы"></a>
+            <a v-else href="javascript:void(0)" @click="tab_opts = true" class="ctrl-buttons date fa fa-reorder" title="настройки таблицы"></a>
             <div class="drop-opt" v-if="table_opts == true">
                 <template v-for="(opt,ind) in table_opts">
                     <label for="'opt'+ind">
@@ -36,11 +44,11 @@
     </div>
     <div class="main-table-wrp">
         <div class="result-wrp" v-for="(art,ind) in results" v-cloak>
-            <div class="section date" >@{{ art.time }}</div>
-            <div class="section rait" >@{{ art.rate }}</div>
-            <div class="section key-words"> <span v-for="(itm,$k) in art.keywords" v-if="$k < 5">@{{ itm }}</span></div>
-            <div class="section desc" title="result-date">@{{ art.text }}</div>
-            <div class="section link" title="result-date">@{{ art.link }}</div>
+            <div class="section date" v-if="table_opts.time.val">@{{ art.time }}</div>
+            <div :class="'section rait '+colored(art.rate)" v-if="table_opts.rate.val" >@{{ art.rate }}</div>
+            <div class="section keywords" v-if="table_opts.keywords.val"> <span v-for="(itm,$k) in art.keywords" v-if="$k < 5">@{{ itm }}</span></div>
+            <div class="section desc" v-if="table_opts.text.val" title="result-date">@{{ art.text }}</div>
+            <div class="section link" v-if="table_opts.link.val" title="result-date"><a :href="art.link">Источник Lenta.ru</a></div>
             <div class="section soc-btn">
                 <div class="fa fa-chevron-down"></div>
                 <div class="fa fa-exchange" title="сравнить"></div>
