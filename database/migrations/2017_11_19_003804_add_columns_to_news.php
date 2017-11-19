a5775469bb1621fa568dd25e6c5af05e690e019a
deixman1 <deix@yandex.ru>
2017-11-19 08:00:52 +0700

    улучшение парсера + парсинг доп инфы

---
+++ database/migrations/2017_11_19_003804_add_columns_to_news.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToNews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->string('link');
+            $table->string('time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
